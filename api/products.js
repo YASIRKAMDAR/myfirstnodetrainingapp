888941d479b6f68afc3a807c75c4e7bd9fe686c1
Mohammed Yasir Kamdar <YASIRKAMDAR@LIVE.COM>
2018-02-07 15:45:42 +0400

    session 3

--- api/products.js
+++ api/products.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-var { gettop4products } = require('../lib/products');
+var { gettop4products, getproductsbyname } = require('../lib/products');
 
 router.all("/*",function (req, res, next){
     next();
@@ -14,5 +14,15 @@ router.get("/listtop", function (req, res, next) {
           res.json(resp);
     });
 });
+
+router.get("/list/:name", function (req, res, next) {
+    getproductsbyname(req.params["name"], function(err,resp) {
+        if(err)
+          res.json({status:"error", message:err.message });
+        else
+          res.json(resp);
+    });
+});
+
 
 module.exports = router;
